8dd2b6582172af8fbed6ee6832bfd2f7f48896d2
Alberto Ibáñez Gamarra <alberiba_94@hotmail.com>
2019-12-24 01:33:45 +0100

    crear y configurar la clase Server en index.ts, clase core de la app

--- server/build/index.js
+++ server/build/index.js
@@ -1,2 +1,32 @@
 "use strict";
-console.log("asdf");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var Server = /** @class */ (function () {
+    function Server() {
+        this.app = express_1.default();
+        this.config();
+        this.routes();
+    }
+    Server.prototype.config = function () {
+        if (typeof process.env.PORT == 'undefined') {
+            this.app.set('port', 3000);
+        }
+        else {
+            this.app.set('port', process.env.PORT);
+        }
+    };
+    Server.prototype.routes = function () {
+    };
+    Server.prototype.start = function () {
+        var _this = this;
+        this.app.listen(this.app.get('port'), function () {
+            console.log('Server iniciado en el puerto ' + _this.app.get('port'));
+        });
+    };
+    return Server;
+}());
+var server = new Server();
+server.start();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,37 @@
-console.log("hola"); 
+import express, {Application} from 'express'; 
+
+class Server {
+    public app : Application; 
+    private port; 
+    constructor(){
+        this.app = express(); 
+        this.config(); 
+        this.routes(); 
+    }
+
+    config():void{
+        if(typeof process.env.PORT == 'undefined' ){
+            this.app.set('port',  3000); 
+        }  
+        else{
+            this.app.set('port',   process.env.PORT); 
+        }
+    }
+
+    routes():void{
+
+    } 
+
+    start():void{
+
+        this.app.listen(this.app.get('port'), () =>{
+            console.log ('Server iniciado en el puerto '  + this.app.get('port')); 
+        }); 
+
+    }
+
+}
+
+
+const server = new Server(); 
+server.start(); 
