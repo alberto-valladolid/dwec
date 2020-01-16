22f60f427143e54bcdeafbad440282f2c99cd480
Alberto <alberiba_94@hotmail.com>
2020-01-16 16:17:45 +0100

    cambios en la bbdd. Nuevas tablas y se han modificado alguna antigua

--- database/database.sql
+++ database/database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE teacher_timetable_db;
 
 USE teacher_timetable_db; 
 
-CREATE TABLE admins(
+CREATE TABLE admin(
     id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_name VARCHAR (100),
     pw VARCHAR (255),
@@ -32,14 +32,41 @@ CREATE TABLE teacher(
     active BOOLEAN DEFAULT 1
 );
 
+/*Los horarios que verá el usuario se sacarán de esta tabla*/
 CREATE TABLE student_teacher(
-    id_teacher INT(6) REFERENCES teacher (id),
-    id_student INT(6) REFERENCES student (id)
-
+    id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_teacher INT(6) NOT NULL REFERENCES teacher (id),
+    id_student INT(6) NOT NULL REFERENCES student (id)
 );
 
-CREATE TABLE class_lost(
+CREATE TABLE absence(
+    id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     id_teacher INT(6) REFERENCES student_teacher (id_teacher),
     id_student INT(6) REFERENCES student_teacher (id_student),
-    class_lost_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    absence_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+/*Para almacenar los días que trabaja el profesor*/
+CREATE TABLE days_of_the_week(
+    id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_teacher INT(6) REFERENCES teacher (id),
+    name VARCHAR (255)
+);
+
+/*Para prefijar las clases que puede dar el profesor*/
+CREATE TABLE available_class(
+    id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_day_of_the_week INT(6) REFERENCES days_of_the_week (id),
+    description VARCHAR (255),
+    class_order INT(3),
+    class_timestamp TIMESTAMP
+);
+
+
+/*Para almacenar las cases que ya han sido reservadas*/
+CREATE TABLE requested_class(
+    id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_available_class INT(6) REFERENCES available_class (id),    
+    repeat_timestamp TIMESTAMP,
+    timestamp TIMESTAMP
 );
